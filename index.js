cd867c6ef551e7d62b4d63f77e4e8268e220b69d
Danilo Ribeiro Abranches dos Santos <danr.dossantos.work@gmail.com>
2026-07-01 00:39:10 -0300

    Add utils dir and config.js file

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
