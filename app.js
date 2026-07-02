fe235619ad8fd37e61f2539e5478db463f348ba8
Danilo Ribeiro Abranches dos Santos <danr.dossantos.work@gmail.com>
2026-07-02 00:41:23 -0300

    Complete app.js file

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URI)
+    .then(connection => {
+        logger.info('connected to MongoDB')
+    }).catch(error => {
+        logger.error(error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
