1d586326a1ac416cff23c694389f79675400e6d2
Danilo Ribeiro Abranches dos Santos <danr.dossantos.work@gmail.com>
2026-07-30 00:13:45 -0300

    feat: add user model and routes for creating and listing users

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -19,6 +20,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
