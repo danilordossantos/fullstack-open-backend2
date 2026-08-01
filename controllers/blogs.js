018d549553b7377cdbb63e8e05796d7df0a1a579
Danilo Ribeiro Abranches dos Santos <danr.dossantos.work@gmail.com>
2026-08-01 18:13:04 -0300

    feat: add user reference to blog and populate user/blogs data

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,17 +1,23 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog.js')
+const User = require('../models/user.js')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
     request.body.likes = request.body.likes || 0
-
-    const blog = new Blog(request.body)
+    const user = await User.findById(request.body.userId)
+    const blog = new Blog({
+        ...request.body,
+        user: user._id
+    })
 
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.status(201).json(savedBlog)
 })
 
