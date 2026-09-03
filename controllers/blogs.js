3c7cf6a41211fe978520d2945b32e4786d3004b8
Danilo Ribeiro Abranches dos Santos <danr.dossantos.work@gmail.com>
2026-09-03 01:40:01 -0300

    fix: populate user data in POST blog response

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -16,6 +16,7 @@ blogsRouter.post('/', userValidator, async (request, response) => {
     })
 
     const savedBlog = await blog.save()
+    await savedBlog.populate('user', { username: 1, name: 1 })
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
     response.status(201).json(savedBlog)
