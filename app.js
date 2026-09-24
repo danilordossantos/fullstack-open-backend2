9c5213423008291e383b1213493078fa206b615d
Danilo Ribeiro Abranches dos Santos <danr.dossantos.work@gmail.com>
2026-09-24 20:14:20 -0300

    chore: configure Fly.io deployment and serve frontend build

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const path = require('path')
 
 const app = express()
 
@@ -18,6 +19,7 @@ mongoose.connect(config.MONGODB_URI)
     })
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.tokenExtractor)
 app.use('/api/blogs', middleware.userExtractor, blogsRouter)
@@ -29,6 +31,15 @@ if (process.env.NODE_ENV === 'test') {
     const testingRouter = require('./controllers/testing')
     app.use('/api/testing', testingRouter)
 }
+
+const serveFrontend = (request, response, next) => {
+    if (request.method === 'GET' && !request.path.startsWith('/api')) {
+        response.sendFile(path.join(__dirname, 'dist', 'index.html'))
+    } else {
+        next()
+    }
+}
+app.use(serveFrontend)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
